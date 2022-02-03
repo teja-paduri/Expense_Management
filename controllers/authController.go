11ff5b5cbbc1114c3c57fc31a119ce050b7e2f85
anusha.chintha <anushachintha06@gmail.com>
2022-02-02 23:25:47 -0500

    git

--- controllers/authController.go
+++ controllers/authController.go
@@ -4,8 +4,8 @@ import (
 	"strconv"
 	"time"
 
-	// "../database"
-	// "../models"
+	"../database"
+	"../models"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gofiber/fiber"
 	"golang.org/x/crypto/bcrypt"
