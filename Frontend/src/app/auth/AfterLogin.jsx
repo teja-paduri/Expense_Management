fa9b2be041003d03c22f798170ecded0e0218213
karthiknakka7 <venkatakartnakka@ufl.edu>
2022-02-04 15:56:00 -0500

    after login page updated
    after login

---
+++ Frontend/src/app/auth/AfterLogin.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+  
+const AfterLogin = (data) => {
+    console.log(data,'vvvvvvvvvvvv');
+  return (
+    <div>
+      <h1>After Login asssssssssssssssssssssssssssssssssssss</h1>
+    </div>
+  );
+};
+  
+export default AfterLogin;
