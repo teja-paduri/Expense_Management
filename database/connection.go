962cb3ab948f906f8a3f4c46a758ce8ef5850a2e
Surya Teja P <suryateja.paduri@ufl.edu>
2022-02-04 23:34:22 -0500

    added database connection

--- database/connection.go
+++ database/connection.go
@@ -1,18 +1,20 @@
 package database
 
 import (
-	"../models"
-	"gorm.io/driver/mysql"
+	"expenseManagement/models"
+
+	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
 func Connect() {
-	connection, err := gorm.Open(mysql.Open("root:rootroot@/yt_go_auth"), &gorm.Config{})
+	// connection, err := gorm.Open(mysql.Open("root:rootroot@/yt_go_auth"), &gorm.Config{})
+	connection, err := gorm.Open(sqlite.Open("expense_management.db"), &gorm.Config{})
 
 	if err != nil {
-		panic("DB connection failed :( !!")
+		panic("DB connection failed !!")
 	}
 
 	DB = connection
