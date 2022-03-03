5ac953b10d78caedf1a069fdbe731fba32725966
Surya Teja P <suryateja.paduri@ufl.edu>
2022-03-03 16:11:42 -0500

    added DB connection

---
+++ database/db.go
@@ -0,0 +1,55 @@
+package database
+
+import (
+	"database/sql"
+	"expenseManagement/models"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+// ExpenseStoreSQL is an implementaton of ExpenseStore in sqlite3
+type ExpenseStoreSQL struct {
+	*sql.DB
+}
+
+// GetUser queries the database for a user and returns it if it's found, nil otherwise
+func (es *ExpenseStoreSQL) GetUser(inputid int) *models.User {
+	var user *models.User
+
+	rows, err := es.Query("SELECT * FROM users WHERE user_id = ?", inputid)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var (
+		id       int64
+		name     string
+		email    string
+		password string
+	)
+
+	for rows.Next() {
+		if err = rows.Scan(&id, &name, &email, &password); err != nil {
+			log.Fatalln(err)
+		}
+		user = &models.User{id, name, email, password}
+	}
+
+	return user
+}
+
+// NewExpenseStoreSQL returns a pointer to an initialized ExpenseStoreSQL
+func NewExpenseStoreSQL() (*ExpenseStoreSQL, error) {
+	e := ExpenseStoreSQL{}
+
+	db, err := sql.Open("sqlite3", "./db/expense-tracker.db")
+	if err != nil {
+		return nil, err
+	}
+
+	e.DB = db
+
+	return &e, nil
+}
