46b4f8f7c6e0a868b30d4433864dbc1a3697950d
anusha.chintha <anushachintha06@gmail.com>
2022-03-05 01:22:30 -0500

    Added Backend Testcases for user authentication

---
+++ controllers/userserver_test.go
@@ -0,0 +1,20 @@
+package controllers
+
+import (
+	"bytes"
+	"controllers"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateEvent(t *testing.T) {
+
+	var jsonstr = []byte(`{"Name":"Karthik","Email":"karthik@gmail.com","Password":"karthik123"}`)
+	w := httptest.NewRecorder()
+	response := httptest.NewRequest("POST", "localhost:8080/user/register", bytes.NewBuffer(jsonstr))
+	controllers.InsertUser(w, response)
+	assert.Equal(t, w.Result().Status, "User Already Exists")
+
+}
