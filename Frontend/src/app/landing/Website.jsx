0272556cfbdaba65381eaa49172187a61109946c
karthiknakka7 <venkatakartnakka@ufl.edu>
2022-03-04 23:05:03 -0500

    modified the home page and layout

--- Frontend/src/app/landing/Website.jsx
+++ Frontend/src/app/landing/Website.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import Image from '/Users/karthik/Documents/SE/Expense_Management/Frontend/src/Image.js';
 
 import LandingLayout from './../layouts/LandingLayout';
 
@@ -7,8 +8,7 @@ const Website = (props) => {
   return (
     <LandingLayout>
       <div className="p-grid p-nogutter p-align-center p-justify-center" style={{ height: '95vh' }}>
-        <img src={require('./../../landing_logo.png')} alt="" style={{ height: '20vh' }} />
-
+        {/* <img src={require('./../../landing_logo.png')} alt="" style={{ height: '20vh' }} /> */}
         <div style={
           {
             marginLeft:20
@@ -19,6 +19,12 @@ const Website = (props) => {
           <p>
             <Link to="/login">Login</Link><span className="color-title"> | </span><Link to="/register">SignUp</Link>
           </p>
+          <Image
+                className=""
+                src={require('./../../assets/images/expense_image.jpeg')}
+                alt="Hero"
+                width={600}
+                height={400} /> 
         </div>
       </div>
     </LandingLayout>
--- Frontend/src/app/layouts/LandingLayout.jsx
+++ Frontend/src/app/layouts/LandingLayout.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 
 const LandingLayout = (props) => {
   return (
-    <div className="layout-main">
+    
+    <div className="layout-main" style= {{backgroundColor:'black'}}>
       {props.children}
+      <footer>
+  <p>Author: Hege Refsnes</p>
+  <p><a href="mailto:hege@example.com">hege@example.com</a></p>
+</footer>
     </div>
+    
   );
 }
 
