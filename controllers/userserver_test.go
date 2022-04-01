32d13b57be967944ebff4336ab1d1324248ac7aa
anusha.chintha <anushachintha06@gmail.com>
2022-04-01 15:18:19 -0400

    added new testcase

--- controllers/userserver_test.go
+++ controllers/userserver_test.go
@@ -8,12 +8,21 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCreateEvent(t *testing.T) {
+func TestCreateEvent1(t *testing.T) {
 
 	var jsonstr = []byte(`{"Email":"karthik@gmail.com","Password":"karthik123"}`)
 	w := httptest.NewRecorder()
 	response := httptest.NewRequest("POST", "localhost:8080/user/login", bytes.NewBuffer(jsonstr))
 	LoginUser(w, response)
+	assert.Equal(t, w.Result().Status, "200 OK")
+
+}
+func TestCreateEvent2(t *testing.T) {
+
+	var jsonstr = []byte(`{"Name":"Anusha","Email":"anusha@gmail.com","Password":"anushachintha"}`)
+	w := httptest.NewRecorder()
+	response := httptest.NewRequest("POST", "localhost:8080/user/register", bytes.NewBuffer(jsonstr))
+	InsertUser(w, response)
 	assert.Equal(t, w.Result().Status, "200 OK")
 
 }
