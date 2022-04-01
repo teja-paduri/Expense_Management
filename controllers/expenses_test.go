c17846b4645d554158e24d315fd92651f2f70e7c
Surya Teja P <suryateja.paduri@ufl.edu>
2022-04-01 16:48:39 -0400

    modified expense unit test cases

---
+++ controllers/expenses_test.go
@@ -0,0 +1,46 @@
+package controllers
+
+import (
+	"bytes"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateExpense1(t *testing.T) {
+
+	var jsonstr = []byte(`{"Name":"springbreak","category":"food","userid":"4","amount":"100"}`)
+	w := httptest.NewRecorder()
+	response := httptest.NewRequest("POST", "localhost:8080/expense", bytes.NewBuffer(jsonstr))
+	InsertExpense(w, response)
+	assert.Equal(t, w.Result().Status, "Expense Created")
+
+}
+func TestCreateExpense2(t *testing.T) {
+
+	var jsonstr = []byte(`{"Name":"springbreak","category":"food","userid":"4","amount":"100"}`)
+	w := httptest.NewRecorder()
+	response := httptest.NewRequest("POST", "localhost:8080/expense", bytes.NewBuffer(jsonstr))
+	InsertExpense(w, response)
+	assert.Equal(t, w.Result().Status, "Expense Already exists")
+
+}
+func TestUpdateExpense(t *testing.T) {
+
+	var jsonstr = []byte(`{"Name":"springbreak","amount":"200"}`)
+	w := httptest.NewRecorder()
+	response := httptest.NewRequest("POST", "localhost:8080/expense/update", bytes.NewBuffer(jsonstr))
+	UpdateExpense(w, response)
+	assert.Equal(t, w.Result().Status, "Expense updated")
+
+}
+func TestDeleteExpense(t *testing.T) {
+
+	var jsonstr = []byte(`{"Name":"springbreak"}`)
+	w := httptest.NewRecorder()
+	response := httptest.NewRequest("POST", "localhost:8080/expense/remove", bytes.NewBuffer(jsonstr))
+	DeleteExpense(w, response)
+	assert.Equal(t, w.Result().Status, "Expense Deleted")
+
+}
