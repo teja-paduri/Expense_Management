1f4fdc54691f347841278bbb5d8ff1272cfe4a20
anusha.chintha <anushachintha06@gmail.com>
2022-04-02 00:32:38 -0400

    updated the expense model

--- models/expense.go
+++ models/expense.go
@@ -1,10 +1,10 @@
 package models
 
 type Expense struct {
-	Id          int64  `json:"id"`
-	UserId      int64  `json:"userid"`
-	Name        string `json:"name"`
-	Category    string `json:"category"`
-	Description string `json:"description"`
-	Amount      string `json:"amount"`
+	Id          int64   `json:"id"`
+	UserId      int64   `json:"userid"`
+	Name        string  `json:"name"`
+	Category    string  `json:"category"`
+	Description string  `json:"description"`
+	Amount      float64 `json:"amount"`
 }
