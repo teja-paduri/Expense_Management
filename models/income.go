facb45e2ddb009b279f6a147d94df426a5ac60a8
anusha.chintha <anushachintha06@gmail.com>
2022-04-18 00:16:21 -0400

    updated attributes of income relation

--- models/income.go
+++ models/income.go
@@ -1,6 +1,7 @@
 package models
 
 type income struct {
+	ID           int64  `json:"ID"`
 	UserId       int64  `json:"userid"`
 	Amount       int64  `json:"amount"`
 	IncomeSource string `json:"incomesource"`
