89fc5d701634f371e116e8ad87d538e2c93b9f2b
upendarpenmetcha <37012026+upendarpenmetcha@users.noreply.github.com>
2022-04-19 03:31:07 -0400

    Added More Cypress Test Cases for Home, Login page

--- Frontend/cypress/integration/app.spec.js
+++ Frontend/cypress/integration/app.spec.js
@@ -1,25 +1,30 @@
-describe("form submission",()=>{
+import Expense from "../../src/app/expense/Expense"
+
+describe("Home Page",()=>{
     beforeEach(()=>{
-        cy.visit('http://localhost:3000/#/login')  
+        cy.visit('http://localhost:3000/#/')  
     })
-    it("should validate email",()=>{
-        cy.get('#emailInput')
-      .type('fake@email.com')
-      .should('have.value', 'fake@email.com')
-    })
-    it("password should not be empty",()=>{
-        cy.get('#passwordInput')
-      .type('hello')
-      .should('have.value', 'hello')
-    })
-    it("Validate button",()=>{
-        // cy.get('#emailInput')
-        // .type('')
-        // .should('have.value', '')
-        cy.get("#loginButton").click()
-        cy.get("#passwordError").should('eq',"Password field is required.")
+    
+    it("Validate Login button",()=>{
+        cy.visit('http://localhost:3000/#/')
+        cy.get('a[href*="/login"]').click()
+        cy.url().should('eq', 'http://localhost:3000/#/login')
+
         
     })
+    it("Validate Sign Up button",()=>{
+      cy.visit('http://localhost:3000/#/')
+      cy.get('a[href*="/register"]').click() 
+      cy.url().should('eq', 'http://localhost:3000/#/register')     
+  })
+
+  it("Should contain Title Expense Management",()=>{
+    cy.visit('http://localhost:3000/#/')
+    cy.contains('Expense').should('exist')
+    cy.contains('Management').should('exist')   
+  })
+
+  
 
     // it("should submit the form",()=>{
     //     cy.get('#text').type("test")
--- Frontend/cypress/integration/login_spec.js
+++ Frontend/cypress/integration/login_spec.js
@@ -1,12 +1,29 @@
-describe('login', () => {
+describe('login Page', () => {
   beforeEach(() => {
     visitLoginPage()
   })
 
   it('A User logs in and sees a welcome message', () => {
-    loginWith('upendar@example.com', 'passsword')
-    expect(cy.contains('Welcome back Upendar')).to.exist
+    loginWith('karthik@gmail.com', 'karthik123')
+    cy.url().should('eq', 'http://localhost:3000/#/dashboard')
   })
+  it("should validate email",()=>{
+    cy.get('#emailInput')
+  .type('fake@email.com')
+  .should('have.value', 'fake@email.com')
+})
+it("password should not be empty",()=>{
+    cy.get('#passwordInput')
+  .type('hello')
+  .should('have.value', 'hello')
+})
+
+it("Validating Logout button",()=>{
+  cy.visit('http://localhost:3000/#/dashboard')
+  logout()
+  cy.url().should('eq', 'http://localhost:3000/#/register')
+
+})
 
 })
 
@@ -17,8 +34,9 @@ const visitLoginPage = () => {
 const loginWith = (email, password) => {
   cy.get('#emailInput').type(email)
   cy.get('#passwordInput').type(password)
-  cy.get('button').click()
+  //cy.get('button').click()
+  cy.get('#loginButton').click()
 }
 const logout = () => {
-  cy.get('button').click()
+  cy.get('#logout').click()
 }
